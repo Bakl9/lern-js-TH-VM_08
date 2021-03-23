d7c6b99235fc5bb8413c4873022e184e81c672b4
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-03-23 10:23:58 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var currentYear = 2018
+var carName = 'Ford'
+var carYear = 2014
+var carAge = currentYear - carYear 
+
+// если в.м меньше 5 лет ...
+// если в.м больше или равно 5 лет и меньше или больше 10 лет 
+// иначе ошибка 
+
+if (carAge < 5 ) {
+    console.log(carName + ' Младше 5 лет')
+} else if (carAge => 5 && carAge <= 10) {
+    console.log(carAge + ' Машина больше или равно 5 лет и меньше или больше 10 лет ')
+} else {
+    console.log ('Возраст ' + carName + ' Равняется '+ carAge+ ' годам')
+}
